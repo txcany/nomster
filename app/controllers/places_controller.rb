24cc2caad1b03c68741851c9cf16e4fd0f3e857b
Amy Donnelly <akdonnelly@gmail.com>
2019-08-21 23:57:19 +0000

    place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,15 @@
 class PlacesController < ApplicationController
   def index
+    # @places = Place.order(:id).paginate(:page => params[:page], :per_page => 10)
+
     @places = Place.all
   end
+
+  def new
+    @place = Place.new
+  end
+
+
 end 
+
+
