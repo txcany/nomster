d8d41e5e0ae50f632843c4dbafd94a151d4f4ff8
Amy Donnelly <akdonnelly@gmail.com>
2019-08-23 04:44:16 +0000

    edit form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,29 +1,33 @@
 class PlacesController < ApplicationController
-  before_action :authentication_user!, only: [:new, :create]
+    before_action :authentication_user!, only: [:new, :create]
 
-  def index
+    def index
     # @places = Place.order(:id).paginate(:page => params[:page], :per_page => 10)
 
-    @places = Place.all
-  end
+      @places = Place.all
+    end
 
-  def new
-    @place = Place.new
-  end
-  def create
-    current_user.places.create(place_params)
-    redirect_to root_path
-  end
+    def new
+      @place = Place.new
+    end
+    def create
+      current_user.places.create(place_params)
+      redirect_to root_path
+    end
 
-  def show
-    @place = Place.find(params[:id])
-  end
+    def show
+      @place = Place.find(params[:id])
+    end
 
-  private
+    def edit
+      @place = Place.find(params[:id])
+    end
+    
+    private
 
-  def place_params
-    params.require(:place).permit(:name, :description, :address)
-  end
+    def place_params
+      params.require(:place).permit(:name, :description, :address)
+    end
 end 
 
 
