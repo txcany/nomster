ab8d3e26e6fbb3946a9c155ba102f8714928d636
Amy Donnelly <akdonnelly@gmail.com>
2019-08-23 00:05:42 +0000

    users to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :authentication_user!, only: [:new, :create]
+
   def index
     # @places = Place.order(:id).paginate(:page => params[:page], :per_page => 10)
 
@@ -11,7 +13,7 @@ class PlacesController < ApplicationController
 
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
