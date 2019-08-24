0f28d2282774ad240ed9d9afa3fa751faadde883
Amy Donnelly <akdonnelly@gmail.com>
2019-08-24 21:40:43 +0000

    form submission

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ApplicationRecord
   has_many :comments
   
   geocoded_by :address
-  after_validation :geocoded_by
+  after_validation :geocode
   
   validates :name, length: { minimum: 3}
   validates :address, :description, presence: true
