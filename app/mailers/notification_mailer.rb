02bf668e92401faad57f6a1ddf44091372af6430
Amy Donnelly <akdonnelly@gmail.com>
2019-08-31 01:50:00 +0000

    email basic

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ApplicationMailer
+    default from: "no-reply@nomsterapp.com"
+
+    def comment_added
+      mail(to: "AKDonnelly@gmail.com",
+         subject: "A comment has been added to your place")
+
+    end
+
+end
